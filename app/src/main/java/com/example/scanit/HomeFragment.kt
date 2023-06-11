230fe79cd9623c6ec241c8eb0472dbcc013c814b
Josiah Jethro Paquiz <josiajeth@gmail.com>
2023-06-11 19:16:57 +0800

    Done Camera scan barcode

--- app/src/main/java/com/example/scanit/HomeFragment.kt
+++ app/src/main/java/com/example/scanit/HomeFragment.kt
@@ -6,24 +6,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
 
 
 class HomeFragment : Fragment() {
-
+    private lateinit var requestCamera: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        val scanBtn = view.findViewById<ImageButton>(R.id.btnScan)
+
+        // Request camera permission using registerForActivityResult
+        requestCamera = registerForActivityResult(ActivityResultContracts.RequestPermission(),){
+            if(it){
+                val intent = Intent(requireContext(), barcodeScan::class.java)
+                requireContext().startActivity(intent)
+            }else{
+                Toast.makeText(activity,"Permision not Granted",Toast.LENGTH_SHORT).show()
+            }
+        }
+        scanBtn.setOnClickListener(){
+
+            requestCamera?.launch(android.Manifest.permission.CAMERA)
+            Toast.makeText(activity,"hi",Toast.LENGTH_SHORT).show()
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
