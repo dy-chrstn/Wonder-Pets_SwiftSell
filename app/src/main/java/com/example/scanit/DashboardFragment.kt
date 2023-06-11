74221d1b26b49da7b44a6f10ed38e95dbbec3622
dychrstn <dy.cavd@gmail.com>
2023-06-11 08:57:10 +0800

    Dashboard Bug fixed

--- app/src/main/java/com/example/scanit/DashboardFragment.kt
+++ app/src/main/java/com/example/scanit/DashboardFragment.kt
@@ -1,48 +1,22 @@
 package com.example.scanit
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.gms.analytics.ecommerce.Product
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DashboardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DashboardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-
-
-        }
     }
 
     override fun onCreateView(
@@ -58,7 +32,7 @@ class DashboardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val database = FirebaseDatabase.getInstance()
-        val productin = view.findViewById<TextView>(R.id.productIn)
+        val productin = view.findViewById<TextView>(R.id.textViewQuantity)
         val avgstocks = view.findViewById<TextView>(R.id.allstocks)
 //        val reference = database.getReference("Products")
         val databaseReference = FirebaseDatabase.getInstance().getReference()
@@ -104,23 +78,7 @@ class DashboardFragment : Fragment() {
 
     }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DashboardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DashboardFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 
     class ProductAdapter(private val productList: List<com.example.scanit.Product>): RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
@@ -138,8 +96,7 @@ class DashboardFragment : Fragment() {
         }
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val product = productList[position]
-            holder.textViewQuantity.text = product.itemQuantity
-
+            holder.textViewQuantity.text = product.itemQuantity.toString()
         }
 
 
