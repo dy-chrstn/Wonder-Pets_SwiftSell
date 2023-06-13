bea81d6fe0b7578332dc47230c0ba0d0f8b735e8
dychrstn <dy.cavd@gmail.com>
2023-06-13 16:30:46 +0800

    POS Activity Added (Front End)

---
+++ app/src/main/java/com/example/scanit/PosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.scanit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pos)
+    }
+}
