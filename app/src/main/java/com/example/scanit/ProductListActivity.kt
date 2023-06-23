23f8693390c3e9d57074c5596a4d84aea3b7a90f
dychrstn <dy.cavd@gmail.com>
2023-06-23 08:31:08 +0800

    Chip Front End

--- app/src/main/java/com/example/scanit/ProductListActivity.kt
+++ app/src/main/java/com/example/scanit/ProductListActivity.kt
@@ -1,12 +1,18 @@
 package com.example.scanit
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.DataSnapshot
@@ -22,6 +28,9 @@ class ProductListActivity : AppCompatActivity() {
     private lateinit var adapter: ProductAdapter
     private lateinit var database: DatabaseReference
     private lateinit var storage: StorageReference
+    private lateinit var chipGroup: ChipGroup
+    private lateinit var categoryReference: DatabaseReference
+    private lateinit var selectedChip: Chip
 
     private val productList: MutableList<Product> = mutableListOf()
     private val filteredProductList: MutableList<Product> = mutableListOf()
@@ -76,6 +85,7 @@ class ProductListActivity : AppCompatActivity() {
             }
         })
 
+
         // Setting up floating action button
         val fabAddProduct: FloatingActionButton = findViewById(R.id.fabAddProduct)
         fabAddProduct.setOnClickListener {
@@ -98,6 +108,72 @@ class ProductListActivity : AppCompatActivity() {
                 return true
             }
         })
+
+        chipGroup = findViewById(R.id.chip_group)
+        categoryReference = FirebaseDatabase.getInstance().reference.child("Category")
+        selectedChip = findViewById(R.id.chip_all)
+
+        retrieveCategories()
+        setupChipGroupListener()
+
+    }
+
+    private fun setupChipGroupListener() {
+        chipGroup.setOnCheckedChangeListener { group, checkedId ->
+            val chip = group.findViewById<Chip>(checkedId)
+            if (chip != null) {
+                if (chip != selectedChip) {
+                    selectedChip.isChecked = false
+                    selectedChip = chip
+                }
+                selectedChip.setChipBackgroundColorResource(android.R.color.black)
+                // Perform the desired action for the selected chip
+            }
+        }
+
+    }
+
+    private fun retrieveCategories() {
+        categoryReference.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                chipGroup.removeAllViews()
+
+                // Add 'All' chip
+                val allChip = createChip("All", "chip_all")
+                chipGroup.addView(allChip)
+
+                for (childSnapshot in dataSnapshot.children) {
+                    val category = childSnapshot.getValue(String::class.java)
+                    category?.let {
+                        val chip = createChip(it, childSnapshot.key.toString())
+                        chipGroup.addView(chip)
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("ProductListActivity", "Error retrieving categories: ${error.message}")
+            }
+        })
+    }
+
+    private fun createChip(text: String, chipId: String): Chip {
+        val chip = LayoutInflater.from(this).inflate(R.layout.item_chip, chipGroup, false) as Chip
+        chip.text = text
+        chip.id = View.generateViewId()
+        chip.isClickable = true
+        chip.isCheckable = true
+        chip.chipBackgroundColor = ColorStateList.valueOf(Color.parseColor("#FFD691")) // Set initial background color
+        chipGroup.isSingleSelection = true
+
+        chip.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                selectedChip.chipBackgroundColor = ColorStateList.valueOf(Color.parseColor("#FFD691")) // Reset background color of previously selected chip
+                selectedChip = chip
+                chip.chipBackgroundColor = ColorStateList.valueOf(Color.parseColor("#FAC367")) // Set background color of selected chip (black)
+            }
+        }
+
+        return chip
     }
 
     private fun updateFilteredProductList() {
